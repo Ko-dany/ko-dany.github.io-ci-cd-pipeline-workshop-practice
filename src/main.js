b4bcbb667a83ebf7cb578c4bdbf8532320bda0cf
Ko-dany <iamgo910@gmail.com>
2025-12-18 17:24:16 -0500

    Add welcom message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { setupCounter } from "./counter.js";
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>Hi, Dany</h1>
+    <h2>Welcome to the first CI/CD pipeline workshop!</h2>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/ko-dany.github.io-ci-cd-pipeline-workshop/",
+});
